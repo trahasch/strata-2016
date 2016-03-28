b5fd605109f3c47a1529452dbc81fbbac5bcd5a1
vsingh <vsingh@cloudera.com>
2016-03-28 10:40:51 -0700

    Added code for MLLib Linear regression on continuos housing data

---
+++ src/main/scala/com/cloudera/spark/LinearRegression/LinearRegressionMLLib.scala
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cloudera.spark
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.regression.LinearRegressionModel
+import org.apache.spark.mllib.regression.LinearRegressionWithSGD
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.{SparkContext, SparkConf}
+
+object LinearRegressionMLLib {
+
+  case class X(
+                id: String ,price: Double, lotsize: Double, bedrooms: Double,
+                bathrms: Double,stories: Double, driveway: String,recroom: String,
+                fullbase: String, gashw: String, airco: String, garagepl: Double, prefarea: String)
+
+  def main (args: Array[String]) {
+
+    if (args.length < 2) {
+      System.err.println(args.length)
+      System.err.println("Usage: TopicModelling <input data dir> <stopwords file location>")
+    }
+
+    val sparkConf = new SparkConf().setAppName("LinearRegressionMLLib")
+
+    val sc = new SparkContext(sparkConf)
+
+    val data = sc.textFile("/Users/vsingh/MR/strata/datasets/housing/housing.regression")
+
+    val parsedData = data.map { line =>
+      val parts = line.split(',')
+      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+    }.cache()
+
+    val numIterations = 100
+    val stepSize = 0.00000001
+
+    val model = LinearRegressionWithSGD.train(parsedData, numIterations, stepSize)
+
+    val valuesAndPreds = parsedData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+
+    val MSE = valuesAndPreds.map { case (v, p) => math.pow((v - p), 2) }.mean()
+
+  }
+}
