b415911c34231558009e726a756676df03c5f8ec
Jayant Shekhar <jayantshekhar@Jayants-iMac.local>
2016-05-28 21:33:09 -0700

    spam

--- src/main/scala/com/cloudera/spark/spamdetection/Spam.scala
+++ src/main/scala/com/cloudera/spark/spamdetection/Spam.scala
@@ -16,18 +16,16 @@
  */
 
 // scalastyle:off println
-package com.cloudera.spark.mllib
+package com.cloudera.spark.spamdetection
 
 import scala.beans.BeanInfo
-
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
-import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.SQLContext
+import com.cloudera.spark.mllib.SparkConfUtil
+import scala.reflect.runtime.universe
 
 @BeanInfo
 case class SpamDocument(file: String, text: String, label: Double)
